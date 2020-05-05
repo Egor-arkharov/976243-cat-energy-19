9e9c44352b0f07f7c119db6de8688cbe9a473162
Egor-arkharov <egorarkharov@gmail.com>
2020-05-05 17:38:50 +0300

    Автоматизация через Gulp

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,28 @@
-var header = document.querySelector('.header__wrapper');
+var header = document.querySelector('.header');
 var toggleNav = document.querySelector('.header__toggle-nav');
 
+var rangeBefore = document.querySelector('.stats__range--before');
+var rangeAfter = document.querySelector('.stats__range--after');
+var imgBefore = document.querySelector('.stats__img--before');
+var imgAfter = document.querySelector('.stats__img--after');
+
+header.classList.remove('header--nojs');
 
 toggleNav.addEventListener('click', function() {
-  if (header.classList.contains('header__wrapper--opened')) {
-    header.classList.remove('header__wrapper--opened');
+  if (header.classList.contains('header--opened')) {
+    header.classList.remove('header--opened');
   } else {
-    header.classList.add('header__wrapper--opened');
+    header.classList.add('header--opened');
   }
+});
+
+rangeBefore.addEventListener('click', function() {
+  imgBefore.classList.remove('visually-hidden');
+  imgAfter.classList.add('visually-hidden');
+});
+
+rangeAfter.addEventListener('click', function() {
+  imgAfter.classList.remove('stats__img--nojs');
+  imgBefore.classList.add('visually-hidden');
+  imgAfter.classList.remove('visually-hidden');
 });
